3d53390b342552d899e6d81e248f665ea3f5ccaf
MusaJin <mussak949@gmail.com>
2024-10-30 19:12:35 +0300

    upd

--- src/Components/Standart/CenterBlock/CenterBlock.jsx
+++ src/Components/Standart/CenterBlock/CenterBlock.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styles from './CenterBlock.module.css'
 
 function CenterBlock({
@@ -53,28 +52,3 @@ function CenterBlock({
 }
 
 export default CenterBlock
-=======
-import React from "react";
-import classes from './CenterBlock.module.css';
-
-function CenterBlock({ children, ...props }) {
-    return ( 
-        <>
-            <div {...props} className={classes.CenterBlock} style={{
-                width: props.width,
-                height: props.height,
-                gap: props.gap,
-                background: `url(${props.background})`,
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                margin: props.margin,
-                padding: props.padding,
-            }}>
-                {children}
-            </div>
-        </>
-     );
-}
-
-export default CenterBlock;
->>>>>>> 882cfeab6b7575d14f774e433503edade9c06a71
